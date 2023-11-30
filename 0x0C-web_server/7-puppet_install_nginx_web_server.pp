e16d397b4b686b4b5c6cd08583bd717cf6ddd422
GideonBature <infoaboutgideon@gmail.com>
2023-11-30 05:28:04 +0100

    Install Nginx web server (w/ Puppet)

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,26 @@
+# install nginx with puppet
+
+$replace_str="server_name _;\n\n\tlocation \/redirect_me {\n\t\t return 301 https:\/\/www.google.com;\n\t}"
+
+package {'nginx':
+  ensure => 'present',
+}
+
+exec {'install':
+  command  => 'sudo apt-get -y update; sudo apt-get -y install nginx',
+  provider => shell,
+}
+
+exec {'Hello World!':
+  command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
+  provider => shell,
+}
+
+exec {'sudo sed -i "s/server_name _;/$replace_str/" /etc/nginx/sites-enabled/default':
+  provider => shell,
+}
+
+exec {'run':
+  command  => 'sudo service nginx restart',
+  provider => shell,
+}
