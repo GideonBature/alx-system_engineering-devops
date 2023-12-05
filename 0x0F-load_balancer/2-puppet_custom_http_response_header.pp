9ffa9adac9e63c9d4cef2d9900d372c3ff68ff03
GideonBature <infoaboutgideon@gmail.com>
2023-12-05 04:23:09 +0100

    automate the task of creating a custom HTTP header response with puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,17 @@
+# automate creating of a custom HTTP header response
+# using puppet
+
+exec {'update':
+  command => '/usr/bin/apt-get',
+}
+-> package { 'nginx':
+  ensure => 'present',
+}
+-> file_line { 'add_custom_header':
+  path  => '/etc/nginx/sites-enabled/default',
+  match => 'server_name _;',
+  line  => "server_name _;\n\tadd_header X-Served-By \"${hostname}\";",
+}
+-> exec {'run':
+  command => '/usr/sbin/service nginx restart',
+}
