4b89ec1f518d0a481a9eaf9989302e3b31962b21
GideonBature <infoaboutgideon@gmail.com>
2023-12-05 05:30:58 +0100

    fix puppet script #2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -9,8 +9,8 @@ exec {'update':
 }
 -> file_line { 'add_custom_header':
   path  => '/etc/nginx/nginx.conf',
-  match => 'http {',
-  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+  match => 'server_name _;',
+  line  => "server_name _;\n\tadd_header X-Served-By \"${hostname}\";",
 }
 -> exec {'run':
   command => '/usr/sbin/service nginx restart',
