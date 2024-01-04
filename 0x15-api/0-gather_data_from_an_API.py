0a5a73c439e619b636502f4358184fe99090c105
GideonBature <infoaboutgideon@gmail.com>
2024-01-04 18:33:56 +0100

    fix code

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -39,4 +39,4 @@ if __name__ == "__main__":
         if data[i]["userId"] is employeeId:
             if (data[i]["completed"] is True):
                 title = data[i]["title"]
-                print("\t{}".format(title))
+                print("\t", title)
